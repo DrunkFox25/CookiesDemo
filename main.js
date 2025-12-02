beb4787f2d02781550ed22bd12270dd723e3d349
Peter Aravin <112434193+DrunkFox25@users.noreply.github.com>
2025-12-02 16:50:34 +0000

    added a button

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 //https://cookiestore.spec.whatwg.org/
 let doc = document.documentElement;
+
 let text = document.getElementById("text");
+let enter = document.getElementById("enter");
 let msg = document.getElementById("msg");
 
 
@@ -12,10 +14,15 @@ let msg = document.getElementById("msg");
 
 text.value = cookieStore.get('text');
 
+function setCook(){
+    msg.innerHTML = "Cookie Set!";
+    setTimeout(function(){msg.innerHTML = "";}, 5000);
+    cookieStore.set('text', text.value);
+}
+
+
 text.addEventListener('keyup', event => function(){
-    if(event.key === "Enter"){
-        msg.innerHTML = "Cookie Set!";
-        setTimeout(function(){msg.innerHTML = "";}, 5000);
-        cookieStore.set('text', text.value);
-    }
+    if(event.key == "Enter") setCook();
 });
+
+GamepadButton.addEventListener('click', setCook);
